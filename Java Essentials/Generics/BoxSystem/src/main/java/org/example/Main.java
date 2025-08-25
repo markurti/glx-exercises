cad57b604a70fd8649b31dce222490b7e8d61e48
Mark Kurti <kurtimark41@gmail.com>
2025-08-25 22:52:11 +0300

    Sample code in main to test Box method

--- Java Essentials/Generics/BoxSystem/src/main/java/org/example/Main.java
+++ Java Essentials/Generics/BoxSystem/src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a box
+        Box<String> box = new Box<>("surprise");
+
+        // Compare it with different objects
+        System.out.println("Compare object in the box with different objects:");
+        System.out.println(box.inspect("another string"));
+        System.out.println(box.inspect(123));
+        System.out.println(box.inspect(false));
+
 
     }
 }
