64eb01328211bdfe397e7e0311f7ca38c23abd6e
Mark Kurti <kurtimark41@gmail.com>
2025-08-25 23:05:38 +0300

    Sample code for shipment; refactoring

--- Java Essentials/Generics/BoxSystem/src/main/java/org/example/Main.java
+++ Java Essentials/Generics/BoxSystem/src/main/java/org/example/Main.java
@@ -11,6 +11,21 @@ public class Main {
         System.out.println(box.inspect(123));
         System.out.println(box.inspect(false));
 
+        // Create more boxes
+        Box<Integer> box1 = new Box<>(123);
+        Box<Boolean> box2 = new Box<>(false);
 
+        // Create a shipment
+        Shipment shipment = new Shipment();
+        shipment.addBox(box);
+        shipment.addBox(box1);
+        shipment.addBox(box2);
+
+        // Check if shipment can take different objects
+        System.out.println("\nCheck if shipment can take different objects:");
+        System.out.println(shipment.inspectShipment(false));
+        System.out.println(shipment.inspectShipment(123));
+        System.out.println(shipment.inspectShipment("abc"));
+        System.out.println(shipment.inspectShipment('z'));
     }
 }
--- Java Essentials/Generics/BoxSystem/src/main/java/org/example/Shipment.java
+++ Java Essentials/Generics/BoxSystem/src/main/java/org/example/Shipment.java
@@ -3,19 +3,19 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shipment<T> {
-    private final List<Box<T>> boxes;
+public class Shipment {
+    private final List<Box<?>> boxes;
 
     public Shipment() {
-        boxes = new ArrayList<Box<T>>();
+        boxes = new ArrayList<>();
     }
 
-    public void addBox(Box<T> box) {
+    public void addBox(Box<?> box) {
         boxes.add(box);
     }
 
     public <E> boolean inspectShipment(E objectOfAnyType) {
-        for (Box<T> box : boxes) {
+        for (Box<?> box : boxes) {
             if (box.inspect(objectOfAnyType)) {
                 return true;
             }
