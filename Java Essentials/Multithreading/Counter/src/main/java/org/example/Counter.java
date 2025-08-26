b252dd3eb04d78ec50042a74c09109761e32a99d
Mark Kurti <kurtimark41@gmail.com>
2025-08-26 20:30:43 +0300

    Counter class implements Runnable

--- Java Essentials/Multithreading/Counter/src/main/java/org/example/Counter.java
+++ Java Essentials/Multithreading/Counter/src/main/java/org/example/Counter.java
@@ -1,10 +1,17 @@
 package org.example;
 
-public class Counter {
+public class Counter implements Runnable {
     private int count;
 
     public Counter() {
         count = 0;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            increment();
+        }
     }
 
     // Thread safe method
