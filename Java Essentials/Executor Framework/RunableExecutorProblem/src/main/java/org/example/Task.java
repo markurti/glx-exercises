dcdfd438ad9932abc9250a279d994cc1fcac7fa2
Mark Kurti <kurtimark41@gmail.com>
2025-08-26 22:36:39 +0300

    implemented thread sleeping in run()

--- Java Essentials/Executor Framework/RunableExecutorProblem/src/main/java/org/example/Task.java
+++ Java Essentials/Executor Framework/RunableExecutorProblem/src/main/java/org/example/Task.java
@@ -4,5 +4,12 @@ public class Task implements Runnable {
     @Override
     public void run() {
         System.out.println("Thread name: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Thread.currentThread().interrupt();
+        }
     }
 }
