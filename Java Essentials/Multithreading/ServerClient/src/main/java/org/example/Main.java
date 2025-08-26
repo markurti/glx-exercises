47bbde521660a9af3a41ba7e3cb80fdec1eb248e
Mark Kurti <kurtimark41@gmail.com>
2025-08-26 21:19:58 +0300

    sample code

--- Java Essentials/Multithreading/ServerClient/src/main/java/org/example/Main.java
+++ Java Essentials/Multithreading/ServerClient/src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int port = 1234;
+        Server server = new Server(port);
+        server.startServer();
+
+        Client client1 = new Client("Client1", "localhost", port);
+        Client client2 = new Client("Client2", "localhost", port);
+
+        client1.start(new String[]{"Hello from Client1!", "Hey", "This is a message!"});
+        client2.start(new String[]{"Hello from Client2!", "Bye!", "This is another message!"});
     }
 }
