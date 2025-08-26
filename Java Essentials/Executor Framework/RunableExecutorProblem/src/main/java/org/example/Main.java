cccb94c5392457b6f4f53e24070c67cbda5bfb27
Mark Kurti <kurtimark41@gmail.com>
2025-08-26 22:44:58 +0300

    ran 20 tasks and handled executor shutdown

--- Java Essentials/Executor Framework/RunableExecutorProblem/src/main/java/org/example/Main.java
+++ Java Essentials/Executor Framework/RunableExecutorProblem/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        Task task = new Task();
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(task);
+        }
+
+        executorService.shutdown();
+
+        if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+            System.out.println("Terminating tasks forcefully...");
+            executorService.shutdownNow();
+        }
+        if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+            System.err.println("Executor did not terminate.");
+        }
+
+        System.out.println("All tasks finished.");
     }
 }
