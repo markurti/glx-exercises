b8ab720f73920af4bac5c8833fd2dc78aa37fcc6
Mark Kurti <kurtimark41@gmail.com>
2025-08-26 20:31:18 +0300

    thread creation and exception handling

--- Java Essentials/Multithreading/Counter/src/main/java/org/example/Main.java
+++ Java Essentials/Multithreading/Counter/src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Counter counter = new Counter();
+
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Total number of count: " + counter.getCount());
     }
 }
