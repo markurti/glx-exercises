ce9abd5c75ecfd6c80a552482ac9e3ca7c1936a5
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 23:49:50 +0300

    sample code to test insert method

--- Java Essentials/JDBC/FundamentalsJDBCproblem/src/main/java/org/example/Main.java
+++ Java Essentials/JDBC/FundamentalsJDBCproblem/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserDao program = new UserDao();
+
+        try {
+            program.insert(new User(1, "Mark", "mark123@gmail.com"));
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
