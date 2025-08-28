a2990f5930c95c05815200d0bd334317f93c1a50
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 19:08:41 +0300

    Sample code for testing ReflectionUtil class method

--- Java Essentials/Reflection API/ReflectionPractice/src/main/java/org/example/Main.java
+++ Java Essentials/Reflection API/ReflectionPractice/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Mark", 21);
+
+        try {
+            ReflectionUtil.printFieldNamesAndVales(person);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Cannot access field values of the given object: " + e.getMessage());
+        }
     }
 }
