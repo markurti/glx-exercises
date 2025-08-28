221f6070c3bc915dab8fe9d94ca2062cb65fdb69
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 18:21:19 +0300

    Created executor with fixed thread pool size of 5

--- Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
+++ Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         ConcurrentHashMap<String, Integer> storage = new ConcurrentHashMap<>();
+
+        ExecutorService executor = Executors.newFixedThreadPool(5);
     }
 }
