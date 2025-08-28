78e600459b7d1a4e067731731b861fa354a0edb9
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 23:38:42 +0300

    insert method implementation

--- Java Essentials/JDBC/FundamentalsJDBCproblem/src/main/java/org/example/UserDao.java
+++ Java Essentials/JDBC/FundamentalsJDBCproblem/src/main/java/org/example/UserDao.java
@@ -1,24 +1,38 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UserDao {
     private int rowsAffected = 0;
 
     // Get the connection
-    private Statement getConnection() throws SQLException {
+    private Connection getConnection() throws SQLException {
+
         String username = "postgres";
         String password = "password";
         String url = "jdbc:postgresql://localhost:5432/Fundamentals";
-        Connection connection = DriverManager.getConnection(url, username, password);
-        return connection.createStatement();
+        return DriverManager.getConnection(url, username, password);
     }
 
-    public static void insert(User user) throws SQLException {
+    public void insert(User user) throws SQLException {
+        String insertQuery = "INSERT INTO Users (id, name, email) VALUES (?, ?, ?)";
 
+        // Try with resources
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(insertQuery)) {
+
+            // Set parameters
+            stmt.setInt(1, user.getId());
+            stmt.setString(2, user.getName());
+            stmt.setString(3, user.getEmail());
+
+            // Execute insert query
+            rowsAffected = stmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Successfully added new user to database.");
+            }
+        }
     }
 
     public static void update(User user) throws SQLException {
