24cb3c1769f5fbde529c57ae4548a8f313ecafd7
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 18:25:15 +0300

    Giving 100 tasks to executor; implemented shutdown for executor

--- Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
+++ Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
@@ -3,11 +3,25 @@ package org.example;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConcurrentHashMap<String, Integer> storage = new ConcurrentHashMap<>();
 
         ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(new Task(storage, "abc", 123));
+        }
+
+        executor.shutdown();
+        if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+            System.out.println("Executor did not terminate: Beginning forceful shutdown...");
+            executor.shutdownNow();
+        }
+        if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+            System.err.println("Executor did not terminate after forceful shutdown attempt.");
+        }
     }
 }
