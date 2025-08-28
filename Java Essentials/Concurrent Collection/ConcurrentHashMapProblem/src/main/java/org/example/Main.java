5b6143633f0a0213a77b38fc42eaaa4275c5d41b
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 18:07:42 +0300

    Create a ConcurrentHashMap

--- Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
+++ Java Essentials/Concurrent Collection/ConcurrentHashMapProblem/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ConcurrentHashMap<String, Integer> storage = new ConcurrentHashMap<>();
     }
 }
