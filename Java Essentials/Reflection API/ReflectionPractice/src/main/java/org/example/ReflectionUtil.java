bd258e6c27652262a8f41177a508350421af2ce1
Mark Kurti <kurtimark41@gmail.com>
2025-08-28 19:27:35 +0300

    Implemented invokePrivateMethod(); exceptions handled

--- Java Essentials/Reflection API/ReflectionPractice/src/main/java/org/example/ReflectionUtil.java
+++ Java Essentials/Reflection API/ReflectionPractice/src/main/java/org/example/ReflectionUtil.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,6 +18,30 @@ public class ReflectionUtil {
             // Set field accessible (if it was private it would throw an IllegalAccessException)
             field.setAccessible(true);
             System.out.println(field.getName() + ":" + field.get(obj));
+        }
+    }
+
+    public static void invokePrivateMethod(Object obj, String methodName, Object... args) throws InvocationTargetException, IllegalAccessException {
+        Class<?> objClass = obj.getClass();
+        Method[] methods = objClass.getDeclaredMethods();
+
+        // If method doesn't exist in given object -> return error message
+        if (Arrays.stream(methods).noneMatch(method -> method.getName().equals(methodName))) {
+            System.out.println("There is no method named: " + methodName + ".");
+            return;
+        }
+
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                // If method is public -> return error message
+                if (method.getModifiers() == Modifier.PUBLIC) {
+                    System.out.println("The method " + methodName + " is public. Please provide a private method name.");
+                    return;
+                }
+                method.setAccessible(true);
+                method.invoke(obj, args);
+                return;
+            }
         }
     }
 }
