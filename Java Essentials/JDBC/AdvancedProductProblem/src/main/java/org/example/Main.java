c698d54592231e266784624201452dde9c00dc14
Mark Kurti <kurtimark41@gmail.com>
2025-09-01 23:28:35 +0300

    More sample code for showcasing

--- Java Essentials/JDBC/AdvancedProductProblem/src/main/java/org/example/Main.java
+++ Java Essentials/JDBC/AdvancedProductProblem/src/main/java/org/example/Main.java
@@ -15,14 +15,25 @@ public class Main {
             program.insert(prod1);
             program.insert(prod2);
             program.insert(prod3);
+            System.out.println();
 
             // Add some stock to product 1
             System.out.println("Old stock in + " + prod1.getName() + ": " + program.getProductStock(1));
             program.addStock(1, 100);
-            System.out.println("New stock in " + prod1.getName() + ": " + program.getProductStock(1));
+            System.out.println("New stock in " + prod1.getName() + ": " + program.getProductStock(1) + '\n');
+
+            // Reduce some stock from product 2
+            System.out.println("Old stock in + " + prod2.getName() + ": " + program.getProductStock(2));
+            program.reduceStock(2, 100);
+            System.out.println("New stock in " + prod2.getName() + ": " + program.getProductStock(2) + '\n');
+
+            // Reduce some stock from product 3
+            System.out.println("Old stock in + " + prod3.getName() + ": " + program.getProductStock(3));
+            program.reduceStock(3, 100);
+            System.out.println("New stock in " + prod3.getName() + ": " + program.getProductStock(3));
 
             // Clear database table to re-run the example
-            //program.clearProducts();
+            program.clearProducts();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
