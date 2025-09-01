eb38fb60253d124204c9a4b9334dcbcaee55272c
Mark Kurti <kurtimark41@gmail.com>
2025-09-01 23:25:00 +0300

    sample code for testing and showcase

--- Java Essentials/JDBC/AdvancedProductProblem/src/main/java/org/example/Main.java
+++ Java Essentials/JDBC/AdvancedProductProblem/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product prod1 = new Product(1, "Apple", 100);
+        Product prod2 = new Product(2, "Banana", 200);
+        Product prod3 = new Product(3, "Orange", 0);
+
+        ProductDao program = new ProductDao();
+
+        try {
+            // Add some sample products to the database
+            program.insert(prod1);
+            program.insert(prod2);
+            program.insert(prod3);
+
+            // Add some stock to product 1
+            System.out.println("Old stock in + " + prod1.getName() + ": " + program.getProductStock(1));
+            program.addStock(1, 100);
+            System.out.println("New stock in " + prod1.getName() + ": " + program.getProductStock(1));
+
+            // Clear database table to re-run the example
+            //program.clearProducts();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
