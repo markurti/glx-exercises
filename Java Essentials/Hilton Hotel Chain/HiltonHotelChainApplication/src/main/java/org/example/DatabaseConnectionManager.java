3d437eb1deecfe433c3b8a61999481df74075d81
Mark Kurti <kurtimark41@gmail.com>
2025-09-05 00:03:18 +0300

    Implemented DatabaseConnectionManager class for easier connection usage

---
+++ Java Essentials/Hilton Hotel Chain/HiltonHotelChainApplication/src/main/java/org/example/DatabaseConnectionManager.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+    private static final String url = "jdbc:postgresql://localhost:5432/HiltonHotelDatabase";
+    private static final String user = "postgres";
+    private static final String password = "password";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to establish database connection: " + e.getMessage());
+        }
+    }
+}
