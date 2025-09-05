406005b18f001a26bdb0efb675780d069ec52a00
Mark Kurti <kurtimark41@gmail.com>
2025-09-05 18:42:30 +0300

    added getters and setters for Reservation fields

--- Java Essentials/Hilton Hotel Chain/HiltonHotelChainApplication/src/main/java/org/example/Entity/Reservation.java
+++ Java Essentials/Hilton Hotel Chain/HiltonHotelChainApplication/src/main/java/org/example/Entity/Reservation.java
@@ -21,4 +21,62 @@ public class Reservation {
         this.status = status;
         this.hotel_id = hotel_id;
     }
+
+    // Getters and setters
+
+    public int getGuest_id() {
+        return guest_id;
+    }
+
+    public void setGuest_id(int guest_id) {
+        this.guest_id = guest_id;
+    }
+
+    public int getReservation_id() {
+        return reservation_id;
+    }
+
+    public void setReservation_id(int reservation_id) {
+        this.reservation_id = reservation_id;
+    }
+
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getHotel_id() {
+        return hotel_id;
+    }
+
+    public void setHotel_id(int hotel_id) {
+        this.hotel_id = hotel_id;
+    }
 }
