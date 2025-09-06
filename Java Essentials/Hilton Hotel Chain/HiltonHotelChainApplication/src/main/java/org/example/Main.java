d8ce1ea26330c73b1e78885939cf080184846678
Mark Kurti <kurtimark41@gmail.com>
2025-09-06 18:57:18 +0300

    program start

--- Java Essentials/Hilton Hotel Chain/HiltonHotelChainApplication/src/main/java/org/example/Main.java
+++ Java Essentials/Hilton Hotel Chain/HiltonHotelChainApplication/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.Interface.ApplicationInterface;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationInterface program = new ApplicationInterface();
+        program.start();
     }
 }
