5818e359c03278d3590c974382c7b645b6eae59a
Mark Kurti <kurtimark41@gmail.com>
2025-09-09 20:58:56 +0300

    Constructor; getters and setters; toString() override

--- Java Testing - JUnit5 And Mockito/AnnotationsAndAssertionsExercise/src/main/java/org/example/Customer.java
+++ Java Testing - JUnit5 And Mockito/AnnotationsAndAssertionsExercise/src/main/java/org/example/Customer.java
@@ -5,4 +5,54 @@ public class Customer {
     private String customerName;
     private String contactNumber;
     private String address;
+
+    public Customer() {}
+
+    public Customer(int custId, String customerName, String contactNumber, String address) {
+        this.custId = custId;
+        this.customerName = customerName;
+        this.contactNumber = contactNumber;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [custId=" + custId +
+                ", customerName=" + customerName +
+                ", contactNumber=" + contactNumber +
+                ", address=" + address + "]";
+    }
 }
