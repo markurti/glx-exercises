024f77bde2ecec1b55a9da43c35888296dd307db
Mark Kurti <kurtimark41@gmail.com>
2025-09-09 21:55:55 +0300

    Implemented constructor + getConnection() method

--- Java Testing - JUnit5 And Mockito/AnnotationsAndAssertionsExercise/src/main/java/org/example/DatabaseConnection.java
+++ Java Testing - JUnit5 And Mockito/AnnotationsAndAssertionsExercise/src/main/java/org/example/DatabaseConnection.java
@@ -1,5 +1,21 @@
 package org.example;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+
+public class DatabaseConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/CustomerDatabase";
+    private static final String user = "postgres";
+    private static final String password = "password";
+
+    public DatabaseConnection() {
+        this(url, user, password);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
