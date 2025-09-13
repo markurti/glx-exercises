12194bd245b1514b355f27ad2b197ff8036d3c16
Mark Kurti <kurtimark41@gmail.com>
2025-09-13 14:21:45 +0300

    Sample menu items

--- Java Beyond Basics/FuncInterfacesAndLambdaExpressions/src/main/java/org/example/Main.java
+++ Java Beyond Basics/FuncInterfacesAndLambdaExpressions/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Sample menu items
+        MenuItem pizza = new MenuItem("Margherita Pizza", 15.99, "MAIN_COURSE");
+        MenuItem salad = new MenuItem("Caesar Salad", 8.50, "APPETIZER");
+        MenuItem tiramisu = new MenuItem("Tiramisu", 6.75, "DESSERT");
+        MenuItem coffee = new MenuItem("Espresso", 3.25, "BEVERAGE");
+        MenuItem mysteryItem = new MenuItem("Special Dish", 12.00, "");
+        MenuItem unknownItem = new MenuItem("Chef's Surprise", 18.00, "EXOTIC");
     }
 }
