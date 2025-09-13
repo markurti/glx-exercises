cf889c4b6b80604fec39271ceb82b84b3f3bd3fe
Mark Kurti <kurtimark41@gmail.com>
2025-09-13 19:03:14 +0300

    Sample code for showcase

--- Java Beyond Basics/AdvancedLanguageFeatures/src/main/java/org/example/Main.java
+++ Java Beyond Basics/AdvancedLanguageFeatures/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Test Older Person Finder");
+        List<Person> people = Arrays.asList(
+                new Person("Alice", 25),
+                new Person("Bob", 30),
+                new Person("Charlie", 35),
+                new Person("Diana", 28),
+                new Person("Mark", 35)
+        );
+
+        if (OldestPersonFinder.findOldestPerson(people).isPresent()) {
+            System.out.println("Found oldest person: " + OldestPersonFinder.findOldestPerson(people).get());
+        }
+
+
     }
 }
