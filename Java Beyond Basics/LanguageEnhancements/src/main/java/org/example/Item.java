90e5cde0596e51d089c9144de2104ae22f5caa03
Mark Kurti <kurtimark41@gmail.com>
2025-09-13 15:32:01 +0300

    Implemented Item class

---
+++ Java Beyond Basics/LanguageEnhancements/src/main/java/org/example/Item.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s ($%.2f)", name, price);
+    }
+}
