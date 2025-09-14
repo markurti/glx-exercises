cac4e92b22823da85fffe50dfe0d7fffd277531e
Mark Kurti <kurtimark41@gmail.com>
2025-09-14 17:19:07 +0300

    decoder and encoder method impl

--- Java Beyond Basics/SecurityAndInteractivity/src/main/java/org/example/SecretKeyGenerator.java
+++ Java Beyond Basics/SecurityAndInteractivity/src/main/java/org/example/SecretKeyGenerator.java
@@ -2,7 +2,9 @@ package org.example;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class SecretKeyGenerator {
 
@@ -14,5 +16,14 @@ public class SecretKeyGenerator {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Failed to generate secret key: " + e.getMessage());
         }
+    }
+
+    public static String keyToString(SecretKey key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static SecretKey stringToKey(String keyString) {
+        byte[] keyBytes = Base64.getDecoder().decode(keyString);
+        return new SecretKeySpec(keyBytes, "AES");
     }
 }
