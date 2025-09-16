7bb39da9a7964648c041b87895670ea79b97e6e9
Mark Kurti <kurtimark41@gmail.com>
2025-09-16 15:39:52 +0300

    demo for multithreading problem

--- Java Design Patterns/Creational Design Pattern/SingletonPattern/src/main/java/org/example/Main.java
+++ Java Design Patterns/Creational Design Pattern/SingletonPattern/src/main/java/org/example/Main.java
@@ -2,6 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Multithreading demo
+        try {
+            Thread thread1 = new Thread(() -> {
+                Logger logger = Logger.getLoggerInstance();
+                System.out.println("Instance from thread 1: " + logger);
+            });
+
+            Thread thread2 = new Thread(() -> {
+                Logger logger = Logger.getLoggerInstance();
+                System.out.println("Instance from thread 2: " + logger);
+            });
+
+            thread1.start();
+            thread2.start();
+
+            thread1.join();
+            thread2.join();
+        } catch (Exception e) {
+            System.out.println("Failed creating instance with threads: " + e.getMessage());
+        }
+
+
     }
 }
