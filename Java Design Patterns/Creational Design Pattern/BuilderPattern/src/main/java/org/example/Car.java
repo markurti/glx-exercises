604e970f745a2eef70d8b6c41c4507f8eccf50d5
Mark Kurti <kurtimark41@gmail.com>
2025-09-16 19:18:47 +0300

    CarBuilder added

--- Java Design Patterns/Creational Design Pattern/BuilderPattern/src/main/java/org/example/Car.java
+++ Java Design Patterns/Creational Design Pattern/BuilderPattern/src/main/java/org/example/Car.java
@@ -8,6 +8,15 @@ public class Car {
     private String transmission;
     private String fuelType;
 
+    private Car(CarBuilder builder) {
+        this.brand = builder.brand;
+        this.model = builder.model;
+        this.color = builder.color;
+        this.engine = builder.engine;
+        this.transmission = builder.transmission;
+        this.fuelType = builder.fuelType;
+    }
+
     public String getBrand() {
         return brand;
     }
@@ -36,5 +45,48 @@ public class Car {
     public String toString() {
         return "Car [brand=" + brand + ", model=" + model + ", color=" + color + ", engine=" + engine
                 + ", transmission=" + transmission + ", fuelType=" + fuelType + "]";
+    }
+
+    public static class CarBuilder {
+        private String brand;
+        private String model;
+        private String color;
+        private String engine;
+        private String transmission;
+        private String fuelType;
+
+        public CarBuilder withBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public CarBuilder withModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public CarBuilder withColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public CarBuilder withEngine(String engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public CarBuilder withTransmission(String transmission) {
+            this.transmission = transmission;
+            return this;
+        }
+
+        public CarBuilder withFuelType(String fuelType) {
+            this.fuelType = fuelType;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
     }
 }
