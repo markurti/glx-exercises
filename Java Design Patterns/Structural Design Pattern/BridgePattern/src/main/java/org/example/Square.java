0eec90eb43e77c65fcba2feabc071401a8986ef2
Mark Kurti <kurtimark41@gmail.com>
2025-09-17 19:09:53 +0300

    Entity class + abstract class implemented

---
+++ Java Design Patterns/Structural Design Pattern/BridgePattern/src/main/java/org/example/Square.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Square extends Shape {
+    private double side;
+
+    public Square(Renderer renderer, double side) {
+        super(renderer);
+        this.side = side;
+    }
+
+    @Override
+    public void draw() {
+        renderer.renderSquare(side);
+    }
+}
