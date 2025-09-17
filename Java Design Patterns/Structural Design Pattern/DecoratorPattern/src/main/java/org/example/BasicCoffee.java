d1186a22a589e21526c7817f33ce0b299ca9cb7f
Mark Kurti <kurtimark41@gmail.com>
2025-09-17 19:27:46 +0300

    BasicCoffee class implemented

---
+++ Java Design Patterns/Structural Design Pattern/DecoratorPattern/src/main/java/org/example/BasicCoffee.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class BasicCoffee implements Coffee {
+
+    @Override
+    public String getDescription() {
+        return "Basic Coffee";
+    }
+
+    @Override
+    public double getCost() {
+        return 2.00;
+    }
+}
