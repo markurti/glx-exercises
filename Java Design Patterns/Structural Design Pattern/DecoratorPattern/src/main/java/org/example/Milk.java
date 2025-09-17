bd6a5281d13747740b4d1b012a269fe97022b976
Mark Kurti <kurtimark41@gmail.com>
2025-09-17 19:29:29 +0300

    Milk concrete decorator implemented

---
+++ Java Design Patterns/Structural Design Pattern/DecoratorPattern/src/main/java/org/example/Milk.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Milk extends CoffeeDecorator {
+    public Milk(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.5;
+    }
+}
