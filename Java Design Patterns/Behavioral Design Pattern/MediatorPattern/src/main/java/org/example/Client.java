cd2616b9013277f4725355aa1a3220b0efb3a6c4
Mark Kurti <kurtimark41@gmail.com>
2025-09-18 16:27:05 +0300

    Demo code in Client

--- Java Design Patterns/Behavioral Design Pattern/MediatorPattern/src/main/java/org/example/Client.java
+++ Java Design Patterns/Behavioral Design Pattern/MediatorPattern/src/main/java/org/example/Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String[] args) {
         ChatRoomMediator chatRoom = new ChatRoomMediator();
 
-        System.out.println("=== Chat Room Demo ===");
+        System.out.println("====Chat Room Demo====");
 
         // Create users
         User alice = new User(chatRoom, "Alice");
