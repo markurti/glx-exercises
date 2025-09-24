cc5b3cb7d6df58010d978ad330a4442a5505b278
Mark Kurti <kurtimark41@gmail.com>
2025-09-24 13:24:32 +0300

    Created project

---
+++ Spring JPA/glologisticsjpa/src/test/java/com/glologistics/glologisticsjpa/GlologisticsjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glologistics.glologisticsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlologisticsjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
