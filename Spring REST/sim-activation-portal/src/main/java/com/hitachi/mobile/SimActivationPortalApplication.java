cb7e20520deedd5500a3b71f882db16265eea2e8
Mark Kurti <kurtimark41@gmail.com>
2025-09-25 14:25:42 +0300

    Hitachi REST API Project

---
+++ Spring REST/sim-activation-portal/src/main/java/com/hitachi/mobile/SimActivationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.hitachi.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimActivationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimActivationPortalApplication.class, args);
+	}
+
+}
---
+++ Spring REST/sim-activation-portal/src/test/java/com/hitachi/mobile/SimActivationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitachi.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimActivationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
