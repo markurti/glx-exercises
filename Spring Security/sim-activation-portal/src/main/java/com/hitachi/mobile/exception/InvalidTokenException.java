66007a29d939f9af84bd4d38f50d3567ee6dd0be
Mark Kurti <kurtimark41@gmail.com>
2025-09-25 22:38:42 +0300

    Added exceptions

---
+++ Spring Security/sim-activation-portal/src/main/java/com/hitachi/mobile/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.hitachi.mobile.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
